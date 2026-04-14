0ad6e20fe425e9f66f7a9a1b5c07f959a3a3a5c3
Daniel Wagner <wagi@kernel.org>
2026-04-14 11:53:40 +0200

    mi: make management interface support optional
    The MI code depends on Linux specific interface (networking) which makes
    porting to other platforms more difficult. Make it optional which also
    helps to reduce the size for a minimal setup.
    
    Signed-off-by: Daniel Wagner <wagi@kernel.org>

--- libnvme/src/nvme/lib.c
+++ libnvme/src/nvme/lib.c
@@ -73,7 +73,9 @@ __public struct libnvme_global_ctx *libnvme_create_global_ctx(FILE *fp, int log_
 __public void libnvme_free_global_ctx(struct libnvme_global_ctx *ctx)
 {
 	struct libnvme_host *h, *_h;
+#ifdef CONFIG_MI
 	libnvme_mi_ep_t ep, tmp;
+#endif
 
 	if (!ctx)
 		return;
@@ -86,8 +88,10 @@ __public void libnvme_free_global_ctx(struct libnvme_global_ctx *ctx)
 
 	libnvme_for_each_host_safe(ctx, h, _h)
 		__libnvme_free_host(h);
+#ifdef CONFIG_MI
 	libnvme_mi_for_each_endpoint_safe(ctx, ep, tmp)
 		libnvme_mi_close(ep);
+#endif
 	free(ctx->config_file);
 	free(ctx->application);
 	libnvme_close_uring(ctx);
